4b39912824bd21892e2d8c1ad2f4d05a6810facd
Javier Alcoba <javier.alcobag@gmail.com>
2025-03-18 23:23:18 -0400

    User Tasks are working well

--- controllers/task.controller.js
+++ controllers/task.controller.js
@@ -19,3 +19,49 @@ exports.getTaskById=async(req,res)=>{
         res.status(200).json(task)
     }
 }
+exports.updateTask=async(req,res)=>{
+  try {
+    const task=await Task.findOne({where: req.body.id});
+    console.log(task)
+    const {title, description, estate,deadline,userId}=req.body;
+    task.title=title || task.title
+    task.description=description || task.description
+    task.estate=estate || task.estate
+    task.deadline= deadline || task.deadline
+    task.userId= userId || task.userId
+    await task.save();
+    res.status(201).json(task);
+  } catch (error) {
+    res.status(404).send("no actualizado")
+  } 
+}
+exports.deleteTask=async(req,res)=>{
+    try {
+        const count= await Task.destroy({ where: {id: parseInt(req.params.id)}});
+        if(count === 0){
+            res.status(404).send("Task not found");
+        }else{
+            res.status(201).json("Task deleted");
+        }
+    } catch (error) {
+        
+    }
+}
+exports.getByIduser=async(req,res)=>{
+    try {
+        const tasks=await Task.findAll({
+            where:{
+                userId:req.params.userId
+            }
+        });
+        if(tasks.length != 0){
+            res.status(201).json(tasks);
+        }else{
+            res.status(404).send("There are not with this user")
+        }
+        
+    } catch (error) {
+        
+        console.error('Error ',error);
+    }
+}
--- routes/task.routes.js
+++ routes/task.routes.js
@@ -1,9 +1,12 @@
 const express= require('express');
 const router= express.Router();
-const {getTasks, createTask, getTaskById}=require('../controllers/task.controller');
+const {getTasks, createTask, getTaskById, updateTask, deleteTask, getByIduser}=require('../controllers/task.controller');
 
 router.get('/tasks',getTasks);
 router.post('/tasks',createTask);
 router.get('/tasks/:id',getTaskById);
+router.put('/tasks/:id',updateTask);
+router.delete('/tasks/:id',deleteTask);
+router.get('/tasksbyuser/:userId',getByIduser);
 
 module.exports=router;
